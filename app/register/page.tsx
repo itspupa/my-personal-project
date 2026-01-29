2a7fe0bb10e9703f5fdf086006bd4971bb7cb53c
Anuna Phiakrathok <anuaq20@gmail.com>
2026-01-29 15:11:42 +0700

    feat: add BackButton component and update SocialLogin layout; enhance SearchBar styling

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import CreateAccout from '../component/card/CreateAccout';
+import BackButton from '../component/button/BackButton';
 
 export default function Register() {
   return (
@@ -9,9 +10,13 @@ export default function Register() {
       className="min-h-screen w-full bg-[#90caf9]"
       style={{ backgroundImage: 'var(--bg-grad-waves)' }}
     >
-      <div className="flex min-h-screen flex-col items-center justify-center px-6 py-16">
+      <div className="w-full px-4 py-3">
+        <BackButton />
+      </div>
+      <div className="flex min-h-[calc(100vh-80px)] flex-col items-center justify-start px-6 pt-4 pb-16">
         <CreateAccout />
       </div>
     </div>
   );
 }
+
