d1fa84d09cb36553fe229b33731accd9021bed10
Anuna Phiakrathok <anuaq20@gmail.com>
2026-02-04 17:06:42 +0700

    refactor: reorganize Notification components and update HeaderUserBar import path

--- app/homepages/page.tsx
+++ app/homepages/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Navbar from '../component/Navbar';
-import HeaderUserBar from '../component/HeaderUserBar';
 import CourseSection from '../component/CourseSection';
 import Footer from '../component/Footer';
 
@@ -9,9 +8,6 @@ export default function Homepage() {
   return (
     <main className="flex min-h-screen flex-col">
       <Navbar />
-      <div className="flex justify-end px-4 py-4">
-        <HeaderUserBar userName="Moodeng ja" />
-      </div>
       <CourseSection />
       <Footer />
     </main>
